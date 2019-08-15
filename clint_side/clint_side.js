1b9bde3badf321a6c323f6c20f3a7a190759d700
montu <montu@gmail.com>
2019-08-15 20:14:37 +0530

    .

--- clint_side/clint_side.js
+++ clint_side/clint_side.js
@@ -1,1 +1,11 @@
 const socket=io()
+const input=document.getElementById('input')
+const btn=document.getElementById('button')
+
+console.log(input)
+
+btn.addEventListener('click',function(){
+    socket.emit('chat',{
+        message:input.value
+    })
+})
--- server.js
+++ server.js
@@ -5,10 +5,16 @@ const http=require('http')
 const socket=require('socket.io')
 
 const server=http.createServer(app)
+const io=socket(server)
 
 app.use(express.static(__dirname+'/clint_side'))
 
-
+io.on('connection',function(socket){
+    console.log('socket id '+socket.id)
+    socket.on('chat',function(data){
+        console.log('message:-'+ data.message)
+    })
+})
 
 server.listen(3000,()=>{
     console.log('connected')
