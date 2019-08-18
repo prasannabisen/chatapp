22906f65bc19b913bd5a5f6de02e85dbf801eaa1
montu <montu@gmail.com>
2019-08-18 20:54:48 +0530

    .

--- clint_side/clint_side.js
+++ clint_side/clint_side.js
@@ -2,15 +2,18 @@ const socket=io()
 const input=document.getElementById('input')
 const btn=document.getElementById('button')
 const list=document.getElementById('list')
+const name=document.getElementById('name')
 
 console.log(list)
 
 btn.addEventListener('click',function(){
     socket.emit('chat',{
+        name:name.value,
         message:input.value
     })
 })
 
 socket.on('chat1',function(data){
-    list.innerHTML +='<li>'+data.message+'</li>'
+    list.innerHTML +=`<p><strong>`+data.name+`</strong>`+":-"+data.message+'</p>'
+    input.value=''
 })
